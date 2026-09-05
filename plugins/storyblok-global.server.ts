5d33bdfb256979734766e821f121c332497bd776
oswy09 <oswyjaimes@gmail.com>
2026-09-04 20:13:14 -0500

    feat: connect home hero texts to Storyblok home story

--- plugins/storyblok-global.server.ts
+++ plugins/storyblok-global.server.ts
@@ -1,11 +1,20 @@
 export default defineNuxtPlugin(async () => {
-  const cached = useState('sb-global', () => ({} as Record<string, string>))
-  if (Object.keys(cached.value).length) return
-  try {
-    const api = useStoryblokApi()
-    const { data } = await api.get('cdn/stories/global', {
-      version: process.env.NODE_ENV === 'production' ? 'published' : 'draft',
-    })
-    cached.value = data?.story?.content || {}
-  } catch {}
+  const version = process.env.NODE_ENV === 'production' ? 'published' : 'draft'
+  const api = useStoryblokApi()
+
+  const sbGlobal = useState('sb-global', () => ({} as Record<string, string>))
+  if (!Object.keys(sbGlobal.value).length) {
+    try {
+      const { data } = await api.get('cdn/stories/global', { version })
+      sbGlobal.value = data?.story?.content || {}
+    } catch {}
+  }
+
+  const sbHome = useState('sb-home', () => ({} as Record<string, string>))
+  if (!Object.keys(sbHome.value).length) {
+    try {
+      const { data } = await api.get('cdn/stories/home', { version })
+      sbHome.value = data?.story?.content || {}
+    } catch {}
+  }
 })
